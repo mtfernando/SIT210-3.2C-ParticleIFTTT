d8577f87c36e8f3f19f378ba5e60d80ebd54fdd6
mtfernando <54390998+mtfernando@users.noreply.github.com>
2020-05-08 20:32:02 +1000

    Update ifttttrial.ino

--- ifttttrial.ino
+++ ifttttrial.ino
@@ -8,4 +8,11 @@ if (PhotoResistor<600){
     Particle.publish("light_level","dark",PRIVATE);
     PhotoResistor = 700;
 }
+else if(PhotoResistor<1000){
+    Particle.publish("light_level", "medium_light", PRIVATE)
+}
+else{
+    Particle.publish("light_level" "bright", PRIVATE)
+}
+    
 }
