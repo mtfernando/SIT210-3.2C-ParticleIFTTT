49592874d7a828cafa4c1c3e0e1011bd779d84ea
mtfernando <54390998+mtfernando@users.noreply.github.com>
2020-05-25 17:17:06 +1000

    Update ifttttrial.ino

--- ifttttrial.ino
+++ ifttttrial.ino
@@ -1,10 +1,17 @@
+#PhotoResistor is a mockup value used to control the publish function.
 int PhotoResistor = 500;
 void setup() {
 
 }
 
 void loop() {
+
+#Mockup value ranges for light levels depending on the value of PhotoResistor.
+#<600 = dark
+#600-1000 = medium_light
+#1000< = bright
 if (PhotoResistor<600){
+    #ligh_level is published once and PhotoResistor is set to 700 so that it would not publish again.
     Particle.publish("light_level","dark",PRIVATE);
     PhotoResistor = 700;
 }
